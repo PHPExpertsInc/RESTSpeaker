ce899b8af15cf84f19a7d0390544cec557a33cb3
Theodore R. Smith <theodore@phpexperts.pro>
2023-01-21 12:22:01 -0600

    [m] Added a composer file for the upcoming PHPUnit v10.
    This references sebastianbergmann/phpunit#5146.

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+clear
+rm -rf composer.lock vendor
+PHP_VERSION=7.2 composer install
+
+time for PHPV in 7.2 7.3 7.4 8.0 8.1 8.2; do 
+    echo "PHP Version: $PHPV"
+    PHP_VERSION=$PHPV composer update
+    PHP_VERSION=$PHPV phpunit
+    read
+done
+
