6b8011df701a0e7eacd50b22bfb5b60ce8dc2a7c
Theodore R. Smith <theodore@phpexperts.pro>
2024-03-29 20:05:27 -0500

    Added PHP 8.3 support.

--- test.sh
+++ test.sh
@@ -4,8 +4,9 @@ clear
 rm -rf composer.lock vendor
 PHP_VERSION=7.2 composer install
 
-time for PHPV in 7.2 7.3 7.4 8.0 8.1 8.2; do 
+time for PHPV in 7.2 7.3 7.4 8.0 8.1 8.2 8.3; do
     echo "PHP Version: $PHPV"
+    PHP_VERSION=$PHPV composer --version
     PHP_VERSION=$PHPV composer update
     PHP_VERSION=$PHPV phpunit
     read
